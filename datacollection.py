d35a60c57af192d2b94d001fee471c1b1265f7f0
V415hT313 <vashu2506rkt@gmail.com>
2024-02-16 23:25:43 +0530

    ---

---
+++ datacollection.py
@@ -0,0 +1,13 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np 
+import math
+import time
+
+cap = cv2.VideoCapture(1)
+detector = HandDetector(maxHands= 1)
+offset = 20
+imgSize = 300
+counter = 0
+
+folder = 
